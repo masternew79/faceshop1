d6d6dd96307f4539f8e096f2d293913484af99d7
mousewip <mousewip@gmail.com>
2017-05-30 22:37:17 +0700

    fixx

--- application/admin/view/product/index.php
+++ application/admin/view/product/index.php
@@ -42,78 +42,74 @@
                 <a href="" class="btn btn-info btn-xs">Chi tiết</a>
             </td>
         </tr>
-        <tr class="text-center" ng-hide="appear2">
-            <td>2</td>
-            <td class="image"><img src="../assets/img/acer1.png" alt="" class="img-responsive"></td>
-            <td>Lorem ipsum dolor sit amet.</td>
-            <td>1000000</td>
-            <td>100000</td>
-            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, architecto.</td>
-            <td>1200</td>
-            <td>
-                <a href="" class="btn btn-default btn-xs" ng-click="appear2 = delete()">Xóa</a>
-                <a href="" class="btn btn-warning btn-xs">Sửa</a>
-                <a href="" class="btn btn-info btn-xs">Chi tiết</a>
-            </td>
-        </tr>
+        <p align="center" style="font-size: 25px;">
+            <?php
+            $sodong = $this->sodong;
+            $tongsotrang=ceil($this->totalPage/$sodong);
+            $nhom = 5;
+            $p = $this->page;
 
-        <tr class="text-center" ng-hide="appear3">
-            <td>3</td>
-            <td class="image"><img src="../assets/img/acer1.png" alt="" class="img-responsive"></td>
-            <td>Lorem ipsum dolor sit amet.</td>
-            <td>1000000</td>
-            <td>100000</td>
-            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, architecto.</td>
-            <td>1200</td>
-            <td>
-                <a href="" class="btn btn-default btn-xs" ng-click="appear3 = delete()">Xóa</a>
-                <a href="" class="btn btn-warning btn-xs">Sửa</a>
-                <a href="" class="btn btn-info btn-xs">Chi tiết</a>
-            </td>
-        </tr>
-        <tr class="text-center" ng-hide="appear3">
-            <td>3</td>
-            <td class="image"><img src="../assets/img/acer1.png" alt="" class="img-responsive"></td>
-            <td>Lorem ipsum dolor sit amet.</td>
-            <td>1000000</td>
-            <td>100000</td>
-            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, architecto.</td>
-            <td>1200</td>
-            <td>
-                <a href="" class="btn btn-default btn-xs" ng-click="appear3 = delete()">Xóa</a>
-                <a href="" class="btn btn-warning btn-xs">Sửa</a>
-                <a href="" class="btn btn-info btn-xs">Chi tiết</a>
-            </td>
-        </tr>
-        <tr class="text-center" ng-hide="appear3">
-            <td>3</td>
-            <td class="image"><img src="../assets/img/acer1.png" alt="" class="img-responsive"></td>
-            <td>Lorem ipsum dolor sit amet.</td>
-            <td>1000000</td>
-            <td>100000</td>
-            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, architecto.</td>
-            <td>1200</td>
-            <td>
-                <a href="" class="btn btn-default btn-xs" ng-click="appear3 = delete()">Xóa</a>
-                <a href="" class="btn btn-warning btn-xs">Sửa</a>
-                <a href="" class="btn btn-info btn-xs">Chi tiết</a>
-            </td>
-        </tr>
-        <tr class="text-center" ng-hide="appear3">
-            <td>3</td>
-            <td class="image"><img src="../assets/img/acer1.png" alt="" class="img-responsive"></td>
-            <td>Lorem ipsum dolor sit amet.</td>
-            <td>1000000</td>
-            <td>100000</td>
-            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, architecto.</td>
-            <td>1200</td>
-            <td>
-                <a href="" class="btn btn-default btn-xs" ng-click="appear3 = delete()" id="btn_delete">Xóa</a>
-                <a href="" class="btn btn-warning btn-xs">Sửa</a>
-                <a href="" class="btn btn-info btn-xs">Chi tiết</a>
-            </td>
-        </tr>
-        <?=$this->totalPage ?>
+            ?>
+            <a style="font-size: 20px;" href="<?=HTP::$baseUrl. '/product'?>"> Đầu </a>&nbsp
+            <?php
+
+            if($p != 1)
+            {
+                ?>
+                <a style="font-size: 20px;" href="<?=HTP::$baseUrl. '/product/'. ($p - 1); ?>"> << </a>&nbsp
+                <?php
+            }
+
+            if($nhom % 2 == 0)
+            {
+                $dau = $p - ($nhom / 2);
+                $cuoi = $p + ($nhom / 2 -1);
+            }
+            else
+            {
+                $dau = $p - floor($nhom / 2);
+                $cuoi = $p + floor($nhom / 2);
+            }
+            if($dau < 1)
+            {
+                $dau = 1;
+                $cuoi = $dau + $nhom - 1;
+            }
+
+            if($cuoi > $tongsotrang)
+            {
+                $cuoi = $tongsotrang;
+                $dau = $cuoi - $nhom + 1;
+            }
+            if($nhom > $tongsotrang)
+            {
+                $dau = 1;
+                $cuoi = $tongsotrang;
+            }
+            for($i=$dau;$i<=$cuoi;$i++)
+            {
+                if ($i==$p)
+                {
+                    echo $i."&nbsp;";
+                }
+                else
+                {
+                    ?>
+                    <a style="font-size: 20px;" href="<?=HTP::$baseUrl. '/product/'. $i; ?></a> <?=$i;?>&nbsp
+                    <?php
+                }
+            }
+
+
+            if($p != $tongsotrang)
+            {
+                ?>
+                <a style="font-size: 20px;" href="<?=HTP::$baseUrl. '/product/'. ($p + 1); ?>">  >> </a>&nbsp
+                <?php
+            }
+            ?>
+            <a style="font-size: 20px;" href="<?=HTP::$baseUrl. '/product/'.  $tongsotrang;?>">  Cuối </a>
+        </p>
 
 
 
